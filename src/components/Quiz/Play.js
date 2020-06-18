52e0aa9a893965ab6e8bfdedf52b868f7c58096d
Kartik Patidar <kartikpatidar815@gmail.com>
2020-06-18 23:27:19 +0530

    Adding button functionality and  associated audio sound

--- src/components/Quiz/Play.js
+++ src/components/Quiz/Play.js
@@ -1,7 +1,12 @@
 import React,{ Component } from 'react';
 import Helmet from 'react-helmet';
+import M from 'materialize-css';
+
 import questions from '../../Questions/questions.json';
 import isEmpty from '../../utils/is-empty';
+import correctSound from '../../assets/audio/correct.mp3';
+import incorrectSound from '../../assets/audio/incorrect.mp3';
+import buttonSound from '../../assets/audio/button-clicked.mp3';
 
 class Play extends Component {
     state = {
@@ -14,7 +19,6 @@ class Play extends Component {
         numberOfAnsweredQuestions: 0,
         currentQuestionIndex: 0,
         score: 0,
-        correctAnswers: 0,
         wrongAnswers: 0,
         hints: 5,
         fiftyFifty: 2,
@@ -40,17 +44,97 @@ class Play extends Component {
                 currentQuestion,
                 nextQuestion,
                 previousQuestion,
+                numberOfQuestions: questions.length,
                 answer
             })
         }
     };
 
+    optionClickHandler = (e) => {
+        if ( e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase() ){
+            document.getElementById('correct-sound').play();
+            this.correctAnswer();
+        } else {
+            document.getElementById('incorrect-sound').play();
+            this.wrongAnswer();
+        }
+    }
+
+    correctAnswer = () => {
+        M.toast({
+            html: 'Correct Answer!',
+            classes: 'toast-valid',
+            displayLength: 500
+        });
+        this.setState(previousState => ({
+            score: previousState.score + 1,
+            currentQuestionIndex: previousState.currentQuestionIndex + 1,
+            numberOfAnsweredQuestions: previousState.numberOfAnsweredQuestions + 1
+        }), () => {
+            this.displayQuestions(this.state.questions, this.state.currentQuestion, this.state.nextQuestion, this.state.previousQuestion);
+        });
+    }
+
+    wrongAnswer = () => {
+        navigator.vibrate(1000); // Vibrates for 1000ms in mobile devices
+        M.toast({
+            html: 'Wrong Answer!',
+            classes: 'toast-invalid',
+            displayLength: 1000
+        });
+        this.setState(previousState => ({
+            wrongAnswers: previousState.wrongAnswers + 1,
+            currentQuestionIndex: previousState.currentQuestionIndex + 1,
+            numberOfAnsweredQuestions: previousState.numberOfAnsweredQuestions + 1
+        }), () => {
+            this.displayQuestions();
+        });
+    }
+
+    nextButtonHandler = () => {
+        if (this.state.nextQuestion !== undefined) {
+            this.playButtonSound();
+            this.setState(prevState => ({
+                currentQuestionIndex: prevState.currentQuestionIndex + 1
+            }), () => {
+                this.displayQuestions();
+            });
+        }
+    }
+
+    prevButtonHandler = () => {
+        if (this.state.currentQuestionIndex !== 0) {
+            this.playButtonSound();
+            this.setState(prevState => ({
+                currentQuestionIndex: prevState.currentQuestionIndex - 1
+            }), () => {
+                this.displayQuestions();
+            });
+        }
+    }
+
+    quitButtonHandler = () => {
+        this.playButtonSound();
+        if(window.confirm('Are you sure you want to quit?')) {
+            this.props.history.push('/');
+        }
+    }
+
+    playButtonSound = () => {
+        document.getElementById('button-sound').play();
+    }
+
     render() {
         
-        const { currentQuestion } = this.state;
+        const { currentQuestion, currentQuestionIndex, numberOfQuestions} = this.state;
         return (
             <>
                 <Helmet><title>Quiz Page</title></Helmet>
+                <>
+                    <audio id="correct-sound" src={correctSound}></audio>
+                    <audio id="incorrect-sound" src={incorrectSound}></audio>
+                    <audio id="button-sound" src={buttonSound}></audio>
+                </>
                 <div className="question">
                     <div>
                         <h2>Quiz Mode</h2>
@@ -67,7 +151,7 @@ class Play extends Component {
                     </div>
                     <div>
                         <p> 
-                            <span className="left">Question :<b> 1</b> of 15 </span>
+                            <span className="left">Question :<b> {currentQuestionIndex + 1}</b> of {numberOfQuestions} </span>
                             <span className="mdi mdi-clock-outline mdi-24px right">
                             <span className="lifeline">2:15</span>
                             </span>
@@ -75,19 +159,19 @@ class Play extends Component {
                     </div>
                     <h5>{currentQuestion.question}</h5>
                     <div className="options-container">
-                        <p className="option">{currentQuestion.optionA}</p>
-                        <p className="option">{currentQuestion.optionB}</p>
+                        <p onClick={this.optionClickHandler} className="option">{currentQuestion.optionA}</p>
+                        <p onClick={this.optionClickHandler} className="option">{currentQuestion.optionB}</p>
                     </div>
                     <div className="options-container">
-                        <p className="option">{currentQuestion.optionC}</p>
-                        <p className="option">{currentQuestion.optionD}</p>
+                        <p onClick={this.optionClickHandler} className="option">{currentQuestion.optionC}</p>
+                        <p onClick={this.optionClickHandler} className="option">{currentQuestion.optionD}</p>
                     </div>
                     <div className="button-container">
-                        <button>Previous</button>
-                        <button>Next</button>
+                        <button onClick={this.prevButtonHandler}>Previous</button>
+                        <button onClick={this.nextButtonHandler}>Next</button>
                     </div>
                     <div className="quit-button-container">
-                        <button>Quit</button> 
+                        <button onClick={this.quitButtonHandler}>Quit</button> 
                     </div>
                 </div>
             </>
